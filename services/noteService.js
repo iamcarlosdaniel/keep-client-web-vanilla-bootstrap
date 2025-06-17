cace1d95675780b852bd8230d7c1b51b2a83689d
Carlos Daniel <c4rl1t0sd4n1e1@gmail.com>
2025-06-17 19:48:07 -0400

    tags

--- services/noteService.js
+++ services/noteService.js
@@ -148,3 +148,111 @@ const unshareNoteWithUser = async (noteId, email) => {
     );
   }
 };
+
+const uploadImageApi = async (formData) => {
+  try {
+    const response = await axiosInstance.post(`/files/upload`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    const message =
+      error?.response?.data?.error?.[0]?.message ||
+      error?.message ||
+      "Unexpected error occurred.";
+
+    console.error("Error adding header image:", message);
+    throw message;
+  }
+};
+
+const addHeaderImage = async (noteId, imageId) => {
+  try {
+    const response = await axiosInstance.put(`/notes/${noteId}/header`, {
+      imageId: imageId,
+    });
+
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    const message =
+      error?.response?.data?.error?.[0]?.message ||
+      error?.message ||
+      "Unexpected error occurred.";
+
+    console.error("Error adding header image:", message);
+    throw message;
+  }
+};
+
+const addTagToNote = async (noteId, tagId) => {
+  try {
+    const response = await axiosInstance.post(`/notes/${noteId}/tags`, {
+      tagId: tagId,
+    });
+
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    console.error(
+      "Error adding tag to note:",
+      error.response.data.error[0].message
+    );
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while adding the tag to the note."
+    );
+  }
+};
+
+const removeTagFromNote = async (noteId, tagId) => {
+  try {
+    const response = await axiosInstance.delete(`/notes/${noteId}/tags`, {
+      data: { tagId: tagId },
+    });
+
+    return {
+      status: response.status,
+      message: response.data.message,
+    };
+  } catch (error) {
+    console.error(
+      "Error removing tag from note:",
+      error.response.data.error[0].message
+    );
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while removing the tag from the note."
+    );
+  }
+};
+
+const getTags = async () => {
+  try {
+    const response = await axiosInstance.get("/tags");
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    console.error("Error fetching tags:", error.response.data.error[0].message);
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while fetching the tags."
+    );
+  }
+};
---
+++ services/tagService.js
@@ -0,0 +1,66 @@
+const createTag = async (tag) => {
+  try {
+    const response = await axiosInstance.post("/tags", tag);
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    console.error("Error creating tag:", error.response.data.error[0].message);
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while creating the tag."
+    );
+  }
+};
+
+const getTags = async () => {
+  try {
+    const response = await axiosInstance.get("/tags");
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    console.error("Error fetching tags:", error.response.data.error[0].message);
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while fetching the tags."
+    );
+  }
+};
+
+const updateTag = async (tagId, tag) => {
+  try {
+    const response = await axiosInstance.put(`/tags/${tagId}`, tag);
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    console.error("Error updating tag:", error.response.data.error[0].message);
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while updating the tag."
+    );
+  }
+};
+
+const deleteTag = async (tagId) => {
+  try {
+    const response = await axiosInstance.delete(`/tags/${tagId}`);
+    return {
+      status: response.status,
+      message: response.data.message,
+    };
+  } catch (error) {
+    console.error("Error deleting tag:", error.response.data.error[0].message);
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while deleting the tag."
+    );
+  }
+};
