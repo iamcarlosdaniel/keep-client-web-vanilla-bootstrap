122f611afdd62ab2c8beb01ba59ef86c0663d811
Carlos Daniel <c4rl1t0sd4n1e1@gmail.com>
2025-06-17 16:33:06 -0400

    note view

--- services/noteService.js
+++ services/noteService.js
@@ -18,6 +18,47 @@ const getAllNotes = async () => {
   }
 };
 
+const getNoteById = async (noteId) => {
+  try {
+    const response = await axiosInstance.get(`/notes/${noteId}`);
+
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    console.error("Error fetching note:", error.response.data.error[0].message);
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while fetching the note."
+    );
+  }
+};
+
+const changeColor = async (noteId, colorId) => {
+  try {
+    const response = await axiosInstance.put(`/notes/${noteId}/color`, {
+      color_id: colorId,
+    });
+
+    return {
+      status: response.status,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    console.error(
+      "Error changing note color:",
+      error.response.data.error[0].message
+    );
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while changing the note color."
+    );
+  }
+};
+
 const createNote = async (nodeData) => {
   try {
     const response = await axiosInstance.post("/notes", nodeData);
@@ -64,6 +105,46 @@ const deleteNoteById = async (noteId) => {
     throw (
       error.response.data.error[0].message ||
       "An error occurred while deleting the note."
+    );
+  }
+};
+
+const share = async (noteId, payload) => {
+  try {
+    const response = await axiosInstance.post(`/notes/${noteId}/share`, {
+      email: payload.email,
+      permission: payload.permission,
+    });
+    return {
+      status: response.status,
+      message: response.data.message,
+    };
+  } catch (error) {
+    console.error("Error sharing note:", error.response.data.error[0].message);
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while sharing the note."
+    );
+  }
+};
+
+const unshare = async (noteId, email) => {
+  try {
+    const response = await axiosInstance.delete(`/notes/${noteId}/share`, {
+      email: email,
+    });
+    return {
+      status: response.status,
+      message: response.data.message,
+    };
+  } catch (error) {
+    console.error(
+      "Error unsharing note:",
+      error.response.data.error[0].message
+    );
+    throw (
+      error.response.data.error[0].message ||
+      "An error occurred while unsharing the note."
     );
   }
 };
